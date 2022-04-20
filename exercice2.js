b481808ab039afd47e3d0e3b5083ee48f355f5dd
rouaamdouni <74329229+rouaamdouni@users.noreply.github.com>
2022-04-20 23:01:08 +0100

    Add files via upload

---
+++ exercice2.js
@@ -0,0 +1,8 @@
+document.writeln('<form id="myForm">');
+document.writeln('<div>');
+document.writeln('<label for="myField">Champ&nbsp;:</label>');
+document.writeln('<input type="text" id="myField" size="60">');
+document.writeln('</div>');
+document.writeln('</form>');
+ 
+document.querySelector('#myField').value="Vous avez réussi votre exercice";
